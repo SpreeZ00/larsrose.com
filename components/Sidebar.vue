b6565789bffcf986267845424bfcfd04655cb03c
SpreeZ <admin@spreez.me>
2022-10-07 23:37:19 +0200

    add sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="relative">
+    <div class="fixed right-0 top-1/2 -mt-3.5 z-10 bg-surface-sidebar p-2 rounded-sm">
+      <div class="grid grid-cols-1 gap-2">
+        <a href="https://www.github.com/SpreeZ00" target="_blank">
+          <GitHubIcon class="text-3xl" />
+        </a>
+        <a href="https://www.discordapp.com/users/377490440644984834" target="_blank">
+          <DiscordIcon class="text-3xl" />
+        </a>
+        <a href="https://www.linkedin.com/in/lars-niklas-rose" target="_blank">
+          <LinkedInIcon class="text-3xl" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import GitHubIcon from '~icons/logos/github-icon'
+import DiscordIcon from '~icons/logos/discord-icon'
+import LinkedInIcon from '~icons/logos/linkedin-icon'
+</script>
