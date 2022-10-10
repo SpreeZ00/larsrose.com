ae14bdb004d42ebababf4c249c323f5ca3d60990
SpreeZ00 <admin@spreez.me>
2022-10-10 22:53:48 +0200

    fix: wrong class

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,13 +1,13 @@
 <template>
-  <nav class="sticky top-0 z-10 h-14 bg-navigation-surface hover:text-surface-sidebar">
+  <nav class="sticky top-0 z-10 h-14 bg-navigation-surface text-surface-text-variant">
     <ul class="flex gap-4 justify-center pt-3">
-      <li class="hover:text-navigation-hover">
+      <li class="hover:text-surface-sidebar">
         <a href="#top">Startseite</a>
       </li>
-      <li class="hover:text-navigation-hover">
+      <li class="hover:text-surface-sidebar">
         <a href="#about">Über mich</a>
       </li>
-      <li class="hover:text-navigation-hover">
+      <li class="hover:text-surface-sidebar">
         <a href="#portfolio">Meine Projekte</a>
       </li>
     </ul>
