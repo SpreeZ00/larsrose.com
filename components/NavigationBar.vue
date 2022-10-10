1cc5fe93f8369e614408dc3995a94131b6db1a72
SpreeZ00 <admin@spreez.me>
2022-10-10 22:40:11 +0200

    fix: wrong class

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="sticky top-0 z-10 h-14 bg-navigation-surface text-surface-text-variant">
+  <nav class="sticky top-0 z-10 h-14 bg-navigation-surface text-surface-sidebar">
     <ul class="flex gap-4 justify-center pt-3">
       <li class="hover:text-navigation-hover">
         <a href="#top">Startseite</a>
